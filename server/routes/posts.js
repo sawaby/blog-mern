405b44d46274cf9905e5b6775df835e8c5b69566
Maria <sawaby@users.noreply.github.com>
2021-07-31 11:47:13 -0700

    adding modules, client and server and database

---
+++ server/routes/posts.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { getposts, createPost} from '../controllers/posts.js';
+const router = express.Router();
+
+router.get('/', getposts);
+router.post('/', createPost);
+export default router;
